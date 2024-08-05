1df38d53afd9e8389d57f57c72e6adb9969a8aaf
DOMCENT\CENTNTB <nicola.temporal@cent-systems.ch>
2024-08-05 08:24:30 +0200

    changed cancellation without error, from cached eagerloaging to buffered

--- NiceExtensions.Enumerable/EagerEnumerable.cs
+++ NiceExtensions.Enumerable/EagerEnumerable.cs
@@ -1,60 +1,42 @@
-﻿using NiceExtensions.Enumerable.Models;
-
-namespace NiceExtensions.Enumerable
+﻿namespace NiceExtensions.Enumerable
 {
 	public static partial class Extensions
 	{
+
 		/// <summary>
-		/// Creates and IEnumerable that caches values while Enumerating and enumerates in the background even if no value is requested at the Time. <br/>
-		/// If streaming a value makes sense to start work without the full working set available but should have the next value ready if the work of and item is complete.
+		/// Creates and IEnumerable that enumerates in the background even if no value is requested at the time. <br/>
+		/// If streaming a value makes sense to start work without the full working set available but should have the next value ready if the work of and item is complete. <br/>
+		/// ExampleUse: IO to IO streaming e.g for parallel reading and writing
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="items"></param>
-		/// <param name="ct">CancellationToken witch cancels the Enumeration in the background (throws)</param>
+		/// <param name="itemsToEnumerateInAdvance">How many items should be enumerated in advance.<br/> Set to -1 for continous loading. Be ware that unconsumed values are cached and use memory :)</param>
+		/// <param name="ct"></param>
 		/// <returns></returns>
-		public static IEnumerable<T> ToCachedEagerLoadingEnumerable<T>(this IEnumerable<T> items, CancellationToken ct = default)
+		/// <exception cref="ArgumentException"></exception>
+		public static IEnumerable<T> ToEagerLoadingEnumerable<T>(this IEnumerable<T> items, int itemsToEnumerateInAdvance = 1, CancellationToken ct = default)
 		{
+			if (itemsToEnumerateInAdvance > 0 && itemsToEnumerateInAdvance != -1) throw new ArgumentException("value was not -1 or > 0", nameof(itemsToEnumerateInAdvance));
+
 			IEnumerator<T> enumerator = items.GetEnumerator();
-			EnumerableCache<T> cache = new(false, new());
+			Queue<T> queue = new();
+			bool finished = false;
 			SemaphoreSlim semaphore = new(1, 1);
-			var task = Task.Run(() =>
-			{
-				while (enumerator.MoveNext())
-				{
-					ct.ThrowIfCancellationRequested();
-					cache.Values.Add(enumerator.Current);
-				}
 
-				cache.Complete = true;
-				enumerator.Dispose();
-				enumerator = null!;
+			var task = Task.Run(async () =>
+			{
+				while (enumerator.MoveNext() && !ct.IsCancellationRequested)
+				{
+					queue.Enqueue(enumerator.Current);
+					while (itemsToEnumerateInAdvance != -1 && queue.Count >= itemsToEnumerateInAdvance && !ct.IsCancellationRequested)
+						await Task.Delay(1);
+				}
+				finished = true;
 			}, ct);
-			return ToCachedEagerLoadingEnumerableHelper(cache, semaphore, task, ct);
-		}
 
-
-
-		private static IEnumerable<T> ToCachedEagerLoadingEnumerableHelper<T>(EnumerableCache<T> cache, SemaphoreSlim semaphore, Task task, CancellationToken ct)
-		{
-			for (int i = 0; true; i++)
+			while (queue.Count != 0 || !finished)
 			{
-				ct.ThrowIfCancellationRequested();
-				if (cache.Values.Count > i)
-					yield return cache.Values[i];
-				else if (cache.Complete)
-					yield break;
-				else
-				{
-					while (cache.Values.Count <= i)
-					{
-						ct.ThrowIfCancellationRequested();
-						if (task.Exception != null)
-							throw task.Exception.InnerException ?? task.Exception;
-						else Task.Delay(1, ct).Wait(ct);
-					}
-
-					yield return cache.Values[i];
-				}
+				yield return queue.Dequeue();
 			}
 		}
 	}

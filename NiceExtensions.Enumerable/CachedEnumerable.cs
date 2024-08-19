14aa7827b60e9822a1bb792be613304a92f9e5e3
DOMCENT\CENTNTB <nicola.temporal@cent-systems.ch>
2024-08-19 08:29:24 +0200

    v 1.1.11

--- NiceExtensions.Enumerable/CachedEnumerable.cs
+++ NiceExtensions.Enumerable/CachedEnumerable.cs
@@ -2,125 +2,193 @@
 
 namespace NiceExtensions.Enumerable
 {
-	public static partial class Extensions
-	{
-		/// <summary>
-		/// Creates and IEnumerable that caches values while Enumerating.<br/>
-		/// If an IEnumerable is used twice or more and a ToList would be a disadvantage and multiple enumeration is counterproductive.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="items"></param>
-		/// <returns></returns>
-		public static IEnumerable<T> ToCachedEnumerable<T>(this IEnumerable<T> items)
-		{
-			IEnumerator<T> enumerator = items.GetEnumerator();
-			EnumerableCache<T> cache = new(false, new());
-			SemaphoreSlim semaphore = new(1, 1);
-			return ToCachedEnumerableHelper(enumerator, cache, semaphore);
-		}
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Creates and IEnumerable that caches values while Enumerating.<br/>
+        /// If an IEnumerable is used twice or more and a ToList would be a disadvantage and multiple enumeration is counterproductive.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ToCachedEnumerable<T>(this IEnumerable<T> items)
+        {
+            IEnumerator<T> enumerator = items.GetEnumerator();
+            EnumerableCache<T> cache = new(false, new());
+            SemaphoreSlim semaphore = new(1, 1);
+            return ToCachedEnumerableHelper(enumerator, cache, semaphore);
+        }
 
-		private static IEnumerable<T> ToCachedEnumerableHelper<T>(IEnumerator<T> enumerator, EnumerableCache<T> cache, SemaphoreSlim semaphore)
-		{
-			for (int i = 0; true; i++)
-			{
-				semaphore.Wait();
-				if (i < cache.Values.Count)
-				{
-					semaphore.Release();
-					yield return cache.Values[i];
-					continue;
-				}
-				T t = default!;
-				bool set = false;
-				try
-				{
-					if (!cache.Complete && enumerator.MoveNext())
-					{
-						t = enumerator.Current;
-						cache.Values.Add(t);
-						set = true;
-					}
-					else
-					{
-						cache.Complete = true;
-						enumerator.Dispose();
-						enumerator = null!;
-					}
-				}
-				catch (Exception)
-				{
-					break;
-					throw;
-				}
-				finally
-				{
-					semaphore.Release();
-				}
-				if (set)
-					yield return t;
-				else
-					yield break;
-			}
-		}
+        private static IEnumerable<T> ToCachedEnumerableHelper<T>(IEnumerator<T> enumerator, EnumerableCache<T> cache, SemaphoreSlim semaphore)
+        {
+            for (int i = 0; true; i++)
+            {
+                if (i >= cache.Values.Count)
+                    semaphore.Wait();
 
-		/// <summary>
-		/// Creates and IAsyncEnumerable that caches values while Enumerating.<br/>
-		/// If an IAsyncEnumerable is used twice or more and a ToList would be a disadvantage and multiple enumeration is counterproductive.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="items"></param>
-		/// <returns></returns>
-		public static IAsyncEnumerable<T> ToCachedAsyncEnumerable<T>(this IAsyncEnumerable<T> items)
-		{
-			IAsyncEnumerator<T> enumerator = items.GetAsyncEnumerator();
-			EnumerableCache<T> cache = new(false, new());
-			SemaphoreSlim semaphore = new(1, 1);
-			return ToCachedAsyncEnumerableHelper(enumerator, cache, semaphore);
-		}
+                if (i < cache.Values.Count)
+                {
+                    yield return cache.Values[i];
+                    continue;
+                }
 
-		private static async IAsyncEnumerable<T> ToCachedAsyncEnumerableHelper<T>(IAsyncEnumerator<T> enumerator, EnumerableCache<T> cache, SemaphoreSlim semaphore)
-		{
-			for (int i = 0; true; i++)
-			{
-				await semaphore.WaitAsync();
-				if (i < cache.Values.Count)
-				{
-					semaphore.Release();
-					yield return cache.Values[i];
-					continue;
-				}
-				T t = default!;
-				bool set = false;
-				try
-				{
-					if (await enumerator.MoveNextAsync())
-					{
-						t = enumerator.Current;
-						cache.Values.Add(t);
-						set = true;
-					}
-					else
-					{
-						cache.Complete = true;
-						await enumerator.DisposeAsync();
-						enumerator = null!;
-					}
-				}
-				catch (Exception)
-				{
-					break;
-					throw;
-				}
-				finally
-				{
-					semaphore.Release();
-				}
-				if (set)
-					yield return t;
-				else
-					yield break;
-			}
-		}
+                T t = default!;
+                bool set = false;
+                try
+                {
+                    if (!cache.Complete && enumerator.MoveNext())
+                    {
+                        t = enumerator.Current;
+                        cache.Values.Add(t);
+                        set = true;
+                    }
+                    else
+                    {
+                        cache.Complete = true;
+                        enumerator.Dispose();
+                        enumerator = null!;
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                    throw;
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+                if (set)
+                    yield return t;
+                else
+                    yield break;
+            }
+        }
 
-	}
+
+        /// <summary>
+        /// Creates and IEnumerable that caches values while Enumerating.<br/>
+        /// If an IEnumerable is used twice or more and a ToList would be a disadvantage and multiple enumeration is counterproductive.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static IAsyncEnumerable<T> ToCachedAsyncEnumerable<T>(this IEnumerable<T> items)
+        {
+            IEnumerator<T> enumerator = items.GetEnumerator();
+            EnumerableCache<T> cache = new(false, new());
+            SemaphoreSlim semaphore = new(1, 1);
+            return ToCachedAsyncEnumerableHelper(enumerator, cache, semaphore);
+        }
+
+        private static async IAsyncEnumerable<T> ToCachedAsyncEnumerableHelper<T>(IEnumerator<T> enumerator, EnumerableCache<T> cache, SemaphoreSlim semaphore)
+        {
+            for (int i = 0; true; i++)
+            {
+                if (i >= cache.Values.Count)
+                    await semaphore.WaitAsync();
+
+                if (i < cache.Values.Count)
+                {
+                    yield return cache.Values[i];
+                    continue;
+                }
+
+                T t = default!;
+                bool set = false;
+                try
+                {
+                    if (!cache.Complete && enumerator.MoveNext())
+                    {
+                        t = enumerator.Current;
+                        cache.Values.Add(t);
+                        set = true;
+                    }
+                    else
+                    {
+                        cache.Complete = true;
+                        enumerator.Dispose();
+                        enumerator = null!;
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                    throw;
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+                if (set)
+                    yield return t;
+                else
+                    yield break;
+            }
+        }
+
+
+        /// <summary>
+        /// Creates and IAsyncEnumerable that caches values while Enumerating.<br/>
+        /// If an IAsyncEnumerable is used twice or more and a ToList would be a disadvantage and multiple enumeration is counterproductive.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static IAsyncEnumerable<T> ToCachedAsyncEnumerable<T>(this IAsyncEnumerable<T> items)
+        {
+            IAsyncEnumerator<T> enumerator = items.GetAsyncEnumerator();
+            EnumerableCache<T> cache = new(false, new());
+            SemaphoreSlim semaphore = new(1, 1);
+            return ToCachedAsyncEnumerableHelper(enumerator, cache, semaphore);
+        }
+
+        private static async IAsyncEnumerable<T> ToCachedAsyncEnumerableHelper<T>(IAsyncEnumerator<T> enumerator, EnumerableCache<T> cache, SemaphoreSlim semaphore)
+        {
+            for (int i = 0; true; i++)
+            {
+                if (i >= cache.Values.Count)
+                    await semaphore.WaitAsync();
+
+                if (i < cache.Values.Count)
+                {
+                    yield return cache.Values[i];
+                    continue;
+                }
+
+
+                T t = default!;
+                bool set = false;
+                try
+                {
+                    if (await enumerator.MoveNextAsync())
+                    {
+                        t = enumerator.Current;
+                        cache.Values.Add(t);
+                        set = true;
+                    }
+                    else
+                    {
+                        cache.Complete = true;
+                        await enumerator.DisposeAsync();
+                        enumerator = null!;
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                    throw;
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+                if (set)
+                    yield return t;
+                else
+                    yield break;
+            }
+        }
+
+    }
 }
--- NiceExtensions.Enumerable/EagerEnumerable.cs
+++ NiceExtensions.Enumerable/EagerEnumerable.cs
@@ -1,4 +1,7 @@
-﻿namespace NiceExtensions.Enumerable
+﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+namespace NiceExtensions.Enumerable
 {
     public static partial class Extensions
     {
@@ -19,7 +22,7 @@
             if (itemsToEnumerateInAdvance <= 0 && itemsToEnumerateInAdvance != -1) throw new ArgumentException("value was not -1 or > 0", nameof(itemsToEnumerateInAdvance));
 
             IEnumerator<T> enumerator = items.GetEnumerator();
-            Queue<T> queue = new();
+            ConcurrentQueue<T> queue = new();
             bool finished = false;
             SemaphoreSlim semaphore = new(1, 1);
 
@@ -36,10 +39,102 @@
 
             while (queue.Count != 0 || !finished)
             {
+                if (task.Exception != null)
+                    throw task.Exception;
+
                 if (queue.Count == 0)
                     Task.Delay(1, ct).Wait(ct);
                 else
-                    yield return queue.Dequeue();
+                    if (queue.TryDequeue(out var res))
+                    yield return res;
+            }
+        }
+
+
+        /// <summary>
+        /// Creates and IAsyncEnumerable that enumerates in the background even if no value is requested at the time. <br/>
+        /// If streaming a value makes sense to start work without the full working set available but should have the next value ready if the work of and item is complete. <br/>
+        /// ExampleUse: IO to IO streaming e.g for parallel reading and writing
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="itemsToEnumerateInAdvance">How many items should be enumerated in advance.<br/> Set to -1 for continous loading. Be ware that unconsumed values are cached and use memory :)</param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static async IAsyncEnumerable<T> ToEagerLoadingAsyncEnumerable<T>(this IEnumerable<T> items, int itemsToEnumerateInAdvance = 1, [EnumeratorCancellation] CancellationToken ct = default)
+        {
+            if (itemsToEnumerateInAdvance <= 0 && itemsToEnumerateInAdvance != -1) throw new ArgumentException("value was not -1 or > 0", nameof(itemsToEnumerateInAdvance));
+
+            IEnumerator<T> enumerator = items.GetEnumerator();
+            ConcurrentQueue<T> queue = new();
+            bool finished = false;
+
+            var task = Task.Run(async () =>
+            {
+                while (enumerator.MoveNext() && !ct.IsCancellationRequested)
+                {
+                    queue.Enqueue(enumerator.Current);
+                    while (itemsToEnumerateInAdvance != -1 && queue.Count >= itemsToEnumerateInAdvance && !ct.IsCancellationRequested)
+                        await Task.Delay(1);
+                }
+                finished = true;
+            }, ct);
+
+            while (queue.Count != 0 || !finished)
+            {
+                if (task.Exception != null)
+                    throw task.Exception;
+
+                if (queue.Count == 0)
+                    await Task.Delay(1, ct);
+                else
+                    if (queue.TryDequeue(out var res))
+                    yield return res;
+            }
+        }
+
+
+        /// <summary>
+        /// Creates and IEnumerable that enumerates in the background even if no value is requested at the time. <br/>
+        /// If streaming a value makes sense to start work without the full working set available but should have the next value ready if the work of and item is complete. <br/>
+        /// ExampleUse: IO to IO streaming e.g for parallel reading and writing
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="itemsToEnumerateInAdvance">How many items should be enumerated in advance.<br/> Set to -1 for continous loading. Be ware that unconsumed values are cached and use memory :)</param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static async IAsyncEnumerable<T> ToEagerLoadingAsyncEnumerable<T>(this IAsyncEnumerable<T> items, int itemsToEnumerateInAdvance = 1, [EnumeratorCancellation] CancellationToken ct = default)
+        {
+            if (itemsToEnumerateInAdvance <= 0 && itemsToEnumerateInAdvance != -1) throw new ArgumentException("value was not -1 or > 0", nameof(itemsToEnumerateInAdvance));
+
+            IAsyncEnumerator<T> enumerator = items.GetAsyncEnumerator(ct);
+            ConcurrentQueue<T> queue = new();
+            bool finished = false;
+
+            var task = Task.Run(async () =>
+            {
+                while (await enumerator.MoveNextAsync() && !ct.IsCancellationRequested)
+                {
+                    queue.Enqueue(enumerator.Current);
+                    while (itemsToEnumerateInAdvance != -1 && queue.Count >= itemsToEnumerateInAdvance && !ct.IsCancellationRequested)
+                        await Task.Delay(1);
+                }
+                finished = true;
+            }, ct);
+
+            while (queue.Count != 0 || !finished)
+            {
+                if (task.Exception != null)
+                    throw task.Exception;
+
+                if (queue.Count == 0)
+                    await Task.Delay(1, ct);
+                else
+                    if (queue.TryDequeue(out var res))
+                    yield return res;
             }
         }
     }
